30ebc2ec5814b2bed6b6479eca2351a6fdb92130
github username <github email address>
2019-09-18 11:44:21 -0500

    added provider and store

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+import guestsReducer from './reducers/guests'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducer = combineReducers({
+    guests: guestsReducer
+})
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+ReactDOM.render(<Provider store= {store}><App/></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
